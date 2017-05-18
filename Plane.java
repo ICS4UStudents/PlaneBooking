e6f872713eedff5dfbe6792d85b1bda33d3a58fb
Botanahmad <botanahmad12345@gmail.com>
2017-05-18 11:12:34 -0400

    Add files via upload
    Started menu

---
+++ Plane.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Plane{ 
+  public int FULL_PLANE = 10; //Maximum seats
+  String[][] seats = new String[2][5];
+  public static void main(String[] args){
+     /*   do {
+            printMainMenu();
+            int choice = add buffer();       // Gives the user a choice in a menu
+             {
+            case 1:
+                makeSeatReservation(seats);
+                break;
+            case 2:
+                cancel(seats);
+                break;
+           // case 3:
+              //  printSeatingChart(seats);
+             //   break;
+            case 4:
+                done = true;
+                break;
+            }
+ 
+        } while ();
+ 
+  */  }
+  }
