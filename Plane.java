07cf3345fd6793a92677477801c96324b0b726e4
rivailleabai <rivailleaba1223@gmail.com>
2017-05-19 13:41:09 -0400

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -65,7 +65,33 @@ public class Plane{
         if (seatIndex == seats.length) {
             System.out.println("All seats are full. Sorry, for the inconvenience...");
         } else {
-            //Enter Mikes's code for Information
+		//
+            try{
+   System.out.println("Wlecome fly by night airline pls enter your personal information befor the payment. \n first name and last name:");
+   
+   String userName =bufRead.readLine();
+   
+   System.out.println("please enter your address");
+   String useraddress = bufRead.readLine();
+
+   System.out.println("please enter your phone number so we can contecet you after.");
+   String userphonenbString = bufReas.readLine();
+   double phonenumber = Double.parseDouble(userphonenbString);
+
+   System.our.println("you have complte the );
+   
+   
+  }
+  chach(IOException err)
+ {
+ System.out.println("Error reading line");
+ }
+ catch(NumberFormatException err) {
+ System.out.println("enter the wrong tryp of number,try again");
+ } 
+    
+}
+
         }
     }
      /**
