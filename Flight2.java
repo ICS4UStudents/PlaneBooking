1ade099d78b8987635b801543c85397c741defc0
BluesLackSheep <438574207@qq.com>
2017-06-14 23:20:10 -0400

    Update Flight2.java

--- Flight2.java
+++ Flight2.java
@@ -1,1 +1,23 @@
+public class flight2 {
+
+    String firstName;
+	String lastName;
+	String address;
+	String phoneNumber;
+	
+	public void set(String firstName, String lastName, String address, String phoneNumber) {
+		// TODO Auto-generated method stub
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		
+	}
+	
+	public flight2(String f, String l, String a, String p) {
+		// TODO Auto-generated constructor stub
+		this.set(f,l,a,p);
+	}
+}
 
