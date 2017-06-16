7d41874974a9dbf63be3a7fd5fa7a6d1f66c1e5b
Botanahmad <botanahmad12345@gmail.com>
2017-06-15 19:41:44 -0500

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -2676,7 +2676,7 @@ public class Plane{
      */
      
  
-      public static void cancelFights(flight1 A1, flight1 A2, flight1 B1, flight1 B2, flight1 C1, flight1 C2, flight1 D1, flight1 D2, flight1 E1, flight1 E2,flight2 A11, flight2 A22, flight2 B11, flight2 B22, flight2 C11, flight2 C22, flight2 D11, flight2 D22, flight2 E11, flight2 E22,flight3 A111, flight3 A222, flight3 B111, flight3 B222, flight3 C111, flight3 C222, flight3 D111, flight3 D222, flight3 E111, flight3 E222)
+      public static void cancelFlights(flight1 A1, flight1 A2, flight1 B1, flight1 B2, flight1 C1, flight1 C2, flight1 D1, flight1 D2, flight1 E1, flight1 E2,flight2 A11, flight2 A22, flight2 B11, flight2 B22, flight2 C11, flight2 C22, flight2 D11, flight2 D22, flight2 E11, flight2 E22,flight3 A111, flight3 A222, flight3 B111, flight3 B222, flight3 C111, flight3 C222, flight3 D111, flight3 D222, flight3 E111, flight3 E222)
       {
          InputStreamReader inStream = new InputStreamReader(System.in);
 	  BufferedReader bufRead = new BufferedReader(inStream);
